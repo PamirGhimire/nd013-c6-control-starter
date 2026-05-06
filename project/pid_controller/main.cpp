9d7476f145f72f55351e1c79efdde8983e19f1bb
Pamir Ghimire <pamirghimire@gmail.com>
2026-05-06 19:08:54 +0200

    tune pid gains

--- project/pid_controller/main.cpp
+++ project/pid_controller/main.cpp
@@ -216,12 +216,12 @@ int main ()
 
   // initialize pid steer
   PID pid_steer = PID();
-  pid_steer.Init(0.3, 0.0, 0.02, -1.2, 1.2);
+  pid_steer.Init(1, 1, 1, -1.2, 1.2);
 
 
   // initialize pid throttle
   PID pid_throttle = PID();
-  pid_throttle.Init(0.3, 0.0, 0.02, -1.0, 1.0);
+  pid_throttle.Init(1, 1, 1, -1.0, 1.0);
 
   h.onMessage([&pid_steer, &pid_throttle, &new_delta_time, &timer, &prev_timer, &i, &prev_timer](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode)
   {
